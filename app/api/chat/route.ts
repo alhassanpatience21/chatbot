11710def7181880e801a4c41716bf2391cd4dcde
Patience <peaalhassan@gmail.com>
2025-02-16 19:02:49 +0000

    Initial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `You are a highly reliable AI job search assistant.`
+
+export async function POST(req: NextRequest) {
+   
+
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: process.env.OPENAI_API_KEY,
+      defaultHeaders: {
+        "HTTP-Referer": "http://localhost:3000",
+        "X-Title": "Headstarter Gh",
+      },
+    });
+   
+    const data = await req.json();
+    const messages = Array.isArray(data) ? data : [data];
+    
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: systemPrompt }, ...messages],
+      model: "openai/o3-mini-high",
+      stream: true
+    });
+   
+    let accumulatedResponse = "";
+  
+  
+    try {
+    //Handling the Streamed Response
+      for await (const chunk of completion) {
+        const content = chunk.choices[0]?.delta?.content;
+        if (content) {
+          accumulatedResponse += content;
+        }
+      }
+    } catch (error) {
+     //Handling errors
+      console.error("Error:", error);
+      return new NextResponse("Internal Server Error", { status: 500 });
+    }
+    //Sending the Final Response Back to the Client
+    return new NextResponse(accumulatedResponse);
+  }
+  
